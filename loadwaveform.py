67de9b9a91298172ed002105d303765db9f1c917
teddy <49538481+shnhrtkyk@users.noreply.github.com>
2020-08-10 21:03:18 +0900

    Update loadwaveform.py

--- loadwaveform.py
+++ loadwaveform.py
@@ -22,6 +22,31 @@ laspath = "/Volumes/ssd/Dublin/*.las"
 lasfiles = []
 lasfiles = glob.glob(laspath)
 
+for file in files:
+    with open(file) as f:
+        l_strip = [s.strip() for s in f.readlines()]
+#        print(l_strip[0].split())
+#        print(type(l_strip[0].split()))
+    #    l = f.readlines()
+    #    print(type(l))
+    #    print(l[0])
+    length = []
+    waves = []
+    for lasfile in lasfiles:
+        ref_ds = Dataset(lasfile)
+        ref_points = ref_ds._xyz
+        out_labels = ref_ds.labels
+        tree = ckdtree.cKDTree(ref_points[:, 0:2])  # only on 2D
+        xyzc_arr = []
+        wave_arr = []
+        for line in l_strip:
+            line = line.split()
+            print((line[8]))
+        #    print(len(line[9:]))
+            if(line[7] != "no_waveform"):
+                xyz = (line[3:6])
+
+
 for lasfile in lasfiles:
     ref_ds = Dataset(lasfile)
     ref_points = ref_ds._xyz
@@ -47,7 +72,7 @@ for lasfile in lasfiles:
                 xyz = (line[3:6])
 #                print(line[3:6])
                 #lasとマッチング
-                multiples = tree.query_ball_point([xyz[0], xyz[1]], r=0.0001, eps=0.0001) ## xy
+                multiples = tree.query_ball_point([xyz[0], xyz[1]], r=0.001, eps=0.0001) ## xy
                 if(len(multiples)>0):
                     print(multiples)
 #                    xyzc_arr.append()
