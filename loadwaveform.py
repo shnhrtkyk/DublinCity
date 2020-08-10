0794882af024a0da9e0493bb8320083cd0687297
teddy <49538481+shnhrtkyk@users.noreply.github.com>
2020-08-10 15:41:56 +0900

    マッチング

--- loadwaveform.py
+++ loadwaveform.py
@@ -9,6 +9,8 @@ Created on Mon Aug 10 08:07:01 2020
 import numpy as np
 import glob
 import scipy
+from dataset import Dataset
+from scipy.spatial import ckdtree
 
 path = "/Volumes/ssd/nyu_2451_38586_las/T_315500_234500/*.txt"
 files = []
@@ -16,30 +18,48 @@ files = glob.glob(path)
 print(files)
 maxleng=[]
 
-for file in files:
-    with open(file) as f:
-        l_strip = [s.strip() for s in f.readlines()]
-#        print(l_strip[0].split())
-#        print(type(l_strip[0].split()))
-    #    l = f.readlines()
-    #    print(type(l))
-    #    print(l[0])
-    length = []
-    waves = []
-    for line in l_strip:
-        line = line.split()
-    #    print((line[8]))
-    #    print(len(line[9:]))
-        if(line[7] != "no_waveform"):
+laspath = "/Volumes/ssd/Dublin/*.las"
+lasfiles = []
+lasfiles = glob.glob(laspath)
+
+for lasfile in lasfiles:
+    ref_ds = Dataset(lasfile)
+    ref_points = ref_ds._xyz
+    out_labels = ref_ds.labels
+    tree = ckdtree.cKDTree(ref_points[:, 0:2])  # only on 2D
+    xyzc_arr = []
+    wave_arr = []
+    for file in files:
+        with open(file) as f:
+            l_strip = [s.strip() for s in f.readlines()]
+    #        print(l_strip[0].split())
+    #        print(type(l_strip[0].split()))
+        #    l = f.readlines()
+        #    print(type(l))
+        #    print(l[0])
+        length = []
+        waves = []
+        for line in l_strip:
+            line = line.split()
+        #    print((line[8]))
+        #    print(len(line[9:]))
+            if(line[7] != "no_waveform"):
+                xyz = (line[3:6])
+#                print(line[3:6])
+                #lasとマッチング
+                multiples = tree.query_ball_point([xyz[0], xyz[1]], r=0.0001, eps=0.0001) ## xy
+                if(len(multiples)>0):
+                    print(multiples)
+#                    xyzc_arr.append()
+        
+                    length.append(int(line[2]))
+                    waves.append(line[7:])
+        
+#        length = np.array(length)
+#        waves = np.array(waves)
+#        maxleng.append(np.amax(length))
+#        print(np.amax(length))
+#        print(np.amax(waves))
     
-            length.append(int(line[2]))
-            waves.append(line[7:])
-    
-    length = np.array(length)
-    waves = np.array(waves)
-    maxleng.append(np.amax(length))
-    print(np.amax(length))
-    print(np.amax(waves))
-    
-print("max is :")
-print(np.amax(maxleng))
+#print("max is :")
+#print(np.amax(maxleng))
